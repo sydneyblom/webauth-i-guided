3cbd9f9773d5ee982509318e007c7bcbd250db2a
Sydney Blom <sydneyblom@#gmail.com>
2019-11-11 11:33:28 -0600

    Hash

--- api/api-router.js
+++ api/api-router.js
@@ -3,6 +3,10 @@ const router = require('express').Router();
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
+const Users = require('../users/users-model')
+const bcrypt = require('bcryptjs');
+
+
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 
@@ -10,4 +14,14 @@ router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
 
+
+router.post('/hash', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+
+  user.password = hash;
+
+  
+
+})
 module.exports = router;
